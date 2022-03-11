3152904658c3de5365aeae838a03707eb81cbe97
arturjzapater <zapater.artur@gmail.com>
2022-03-11 17:00:08 +0100

    removed body-parser

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
-const bodyParser = require('body-parser')
 const express = require('express')
 const { error } = require('./middleware')
 const { api } = require('./routes')
 
 const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/api', api)
 
